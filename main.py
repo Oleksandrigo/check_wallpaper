c5821e939e159cce47a1ce943323b22da3ce9d1e
Oleksandrigo <oleksandrigo@gmail.com>
2024-04-28 18:28:46 +0300

    init

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 r1 = r"(Get-ItemProperty 'HKCU:\Control Panel\Desktop' TranscodedImageCache -ErrorAction Stop).TranscodedImageCache"
 r2 = r"[System.Text.Encoding]::Unicode.GetString({r1}) -replace '(.+)([A-Z]:[0-9a-zA-Z\\])+','$2'"
-file_name = subprocess.run(
+file_path = subprocess.run(
     [
         'powershell.exe',
         "-NoProfile",
@@ -13,6 +13,6 @@ file_name = subprocess.run(
     capture_output=True,
     text=True).stdout
 
-res = ''.join(i for i in file_name if i.isprintable())
+res = ''.join(i for i in file_path if i.isprintable())
 
 subprocess.Popen(r'explorer /select,{res}'.replace("{res}", res))
